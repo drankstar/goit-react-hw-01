76ad96fe9c2b377052add69fd3b170800214e6c0
drankstar <137785834+drankstar@users.noreply.github.com>
2024-02-11 14:54:00 +0200

    alltask

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,8 +1,9 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
+import styles from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
   return (
-    <ul>
+    <ul className={styles.container}>
       {friends.map((el) => (
         <li key={el.id}>
           <FriendListItem key={el.id} avatar={el.avatar} name={el.name} isOnline={el.isOnline} />
